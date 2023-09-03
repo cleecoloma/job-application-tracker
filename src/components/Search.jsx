d41800128e7022ada875c8dddeca65e506750510
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-03 00:21:51 -0700

    centered search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,22 +10,26 @@ class Search extends React.Component {
   }
 
   render() {
+     const searchStyle = {
+       position: 'fixed',
+       top: '5rem',
+       left: '50%', // To horizontally center the search bar
+       transform: 'translateX(-50%)', // To center it horizontally
+     };
     return (
       <>
-        <Navbar expand="lg" className="bg-body-tertiary mt-5" fixed="top">
-          <Container fluid>
-            <Form className="d-flex justify-content-center mx-auto">
-              <Form.Control
-                type="search"
-                style={{ width:"20rem"}}
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Container>
-        </Navbar>
+        <div style={searchStyle}>
+          <Form className="d-flex mx-auto">
+            <Form.Control
+              type="search"
+              style={{ width: '20rem' }}
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </div>
       </>
     );
   }
