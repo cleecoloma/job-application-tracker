1b68ec76bed38b23ef7ae9ade5e5e2b092f88796
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-10 11:52:56 -0700

    added get http method

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,57 @@ import React from 'react';
 import AddJobModal from './components/AddJobModal';
 import Search from './components/Search';
 import Listings from './components/Listings';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      token: null,
       modalPreview: false,
-      job: {
-        company: null,
-        location: null,
-        title: null,
-        postingLink: null,
-        appliedStatus: false,
-        appliedDate: null,
-        notes: null,
-      },
+      jobs: [],
+      // job: {
+      //   company: null,
+      //   location: null,
+      //   title: null,
+      //   postingLink: null,
+      //   appliedStatus: false,
+      //   appliedDate: null,
+      //   notes: null,
+      // },
     };
+  }
+
+  sendRequest = async (method, token, id, data) => {
+    config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      method,
+      baseUrl: SERVER_URL,
+      url: id ? `/jobs/${id}`: '/jobs',
+      data: data ? data : null,
+    };
+    return await axios(config);
+  }
+
+  async componentDidMount() {
+    const response = await this.props.auth0.getIdTokenClaims();
+    const token = response.__raw;
+    // this.toggleLoading();
+    this.setState({ token }, () => {
+      this.getJobs();
+      // this.toggleLoading();
+    });
   }
 
   toggleModal = () => {
     this.setState({ modalPreview: !this.state.modalPreview });
+  };
+
+  // GET
+  handleGetJobs = async () => {
+    const response = this.sendRequest('GET', this.state.token)
   };
 
   render() {
