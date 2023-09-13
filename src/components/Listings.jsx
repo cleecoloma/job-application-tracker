fd6662e92f99dfb66c059ae149c28aa897f8aa1a
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-12 23:33:08 -0700

    delete method now works

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -67,7 +67,6 @@ class Listings extends React.Component {
     this.setState({
       jobs: [...this.state.jobs, response.data],
     });
-    console.log(jobObject);
   };
 
   // UPDATE
@@ -94,9 +93,10 @@ class Listings extends React.Component {
   handleDeleteJobs = async (id) => {
     const response = await this.sendRequest('DELETE', this.state.token, id);
     const updatedJobs = this.state.jobs.filter((job) => {
-      job.id !== id;
+      job._id !== id;
     });
     this.setState({
+      fullModalPreview: !this.state.fullModalPreview,
       jobs: updatedJobs,
     });
     this.handleGetJobs();
@@ -147,6 +147,7 @@ class Listings extends React.Component {
           fullModalPreview={this.state.fullModalPreview}
           jobs={this.state.showSpecificJob}
           toggleEditModal={this.toggleEditModal}
+          handleDeleteJobs={this.handleDeleteJobs}
         />
         <div className="listings">
           {this.state.jobs.length > 0
@@ -156,8 +157,6 @@ class Listings extends React.Component {
                   jobs={job}
                   toggleFullModal={this.toggleFullModal}
                   fullModalPreview={this.state.fullModalPreview}
-                  toggleEdit={this.toggleEdit}
-                  toggleDelete={this.toggleDelete}
                 />
               ))
             : null}
