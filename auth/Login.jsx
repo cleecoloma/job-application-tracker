fdb1235cf6fa9e742d81e1e6bd5c18ed4b607870
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-14 13:19:31 -0700

    updated css for login and logout buttons

--- auth/Login.jsx
+++ auth/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import Button from 'react-bootstrap/Button';
 
 function Login() {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
@@ -8,6 +9,6 @@ function Login() {
     loginWithRedirect();
   }
 
-  return !isAuthenticated && <button onClick={handleLogin}>Log in</button>;
+  return !isAuthenticated && <Button onClick={handleLogin}>Log in</Button>;
 }
 export default Login;
--- auth/Logout.jsx
+++ auth/Logout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import Button from 'react-bootstrap/Button';
 
 function LogoutButton() {
   const { isAuthenticated, logout } = useAuth0();
@@ -8,7 +9,7 @@ function LogoutButton() {
     logout({ returnTo: window.location.origin });
   }
 
-  return isAuthenticated && <button onClick={handleLogout}>Log out</button>;
+  return isAuthenticated && <Button onClick={handleLogout}>Log out</Button>;
 }
 
 export default LogoutButton;
