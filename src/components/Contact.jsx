67464e13c85110d1b6ea099056f24e321a2837cb
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-14 22:46:56 -0700

    updated color for linkedin and github icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import { Container } from 'react-bootstrap';
 import { withAuth0 } from '@auth0/auth0-react';
 import '../styles/Contact.css';
+import { Linkedin, Github } from 'react-bootstrap-icons';
 
 class Contact extends React.Component {
   render() {
@@ -18,7 +19,9 @@ class Contact extends React.Component {
               src="https://avatars.githubusercontent.com/u/104961833?v=4"
             />
             <Card.Body>
-              <Card.Title id='contact-card-title'>Chester Lee Coloma</Card.Title>
+              <Card.Title id="contact-card-title">
+                Chester Lee Coloma
+              </Card.Title>
               <br />
               <Card.Text>• Software Developer && Mechanical Engineer</Card.Text>
               <br />
@@ -28,31 +31,27 @@ class Contact extends React.Component {
               </Card.Text>
               <br />
               <Card.Text>
-                • Passion for front-end web development with a focus on responsive
-                design and user interface (UI) development
+                • Passion for front-end web development with a focus on
+                responsive design and user interface (UI) development
               </Card.Text>
               <br />
-              <div
-                id="contact-buttons"
-              >
-                <Button variant="dark" className="github-button">
-                  <a
-                    href="https://github.com/cleecoloma"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fab fa-github"></i> GitHub
-                  </a>
-                </Button>
-                <Button variant="light" className="linkedin-button">
-                  <a
-                    href="https://www.linkedin.com/in/chesterleecoloma/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fab fa-linkedin"></i> LinkedIn
-                  </a>
-                </Button>
+              <div id="contact-buttons">
+                <a
+                  className="github-icon"
+                  href="https://github.com/cleecoloma"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Github size={42} />
+                </a>
+                <a
+                  className="linkedin-icon"
+                  href="https://www.linkedin.com/in/chesterleecoloma/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Linkedin size={42} />
+                </a>
               </div>
             </Card.Body>
           </Card>
