5206df0c98754ac18baf2c293bd81b0f33a39b0e
Chester Lee Coloma <cleecoloma@gmail.com>
2023-09-24 14:38:28 -0700

    removed commented code on Contact comp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,52 +7,49 @@ import { Linkedin, Github } from 'react-bootstrap-icons';
 class Contact extends React.Component {
   render() {
     return (
-        <Container className="centered-container">
-          <h2>Contact The Developer</h2>
-          <Card id="contact-card">
-            <Card.Img
-              variant="top"
-              src="https://avatars.githubusercontent.com/u/104961833?v=4"
-            />
-            <Card.Body>
-              <Card.Title id="contact-card-title">
-                Chester Lee Coloma
-              </Card.Title>
-              <br />
-              <Card.Text>• Software Developer && Mechanical Engineer</Card.Text>
-              <br />
-              <Card.Text>
-                • Previous experience - planned and executed maintenance repairs
-                of US Navy nuclear submarines
-              </Card.Text>
-              <br />
-              <Card.Text>
-                • Passion for front-end web development with a focus on
-                responsive design and user interface (UI) development
-              </Card.Text>
-              <br />
-              <div id="contact-buttons">
-                <a
-                  className="github-icon"
-                  href="https://github.com/cleecoloma"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Github size={42} />
-                </a>
-                <a
-                  className="linkedin-icon"
-                  href="https://www.linkedin.com/in/chesterleecoloma/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Linkedin size={42} />
-                </a>
-              </div>
-            </Card.Body>
-          </Card>
-        </Container>
-      // )
+      <Container className="centered-container">
+        <h2>Contact The Developer</h2>
+        <Card id="contact-card">
+          <Card.Img
+            variant="top"
+            src="https://avatars.githubusercontent.com/u/104961833?v=4"
+          />
+          <Card.Body>
+            <Card.Title id="contact-card-title">Chester Lee Coloma</Card.Title>
+            <br />
+            <Card.Text>• Software Developer && Mechanical Engineer</Card.Text>
+            <br />
+            <Card.Text>
+              • Previous experience - planned and executed maintenance repairs
+              of US Navy nuclear submarines
+            </Card.Text>
+            <br />
+            <Card.Text>
+              • Passion for front-end web development with a focus on responsive
+              design and user interface (UI) development
+            </Card.Text>
+            <br />
+            <div id="contact-buttons">
+              <a
+                className="github-icon"
+                href="https://github.com/cleecoloma"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github size={42} />
+              </a>
+              <a
+                className="linkedin-icon"
+                href="https://www.linkedin.com/in/chesterleecoloma/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Linkedin size={42} />
+              </a>
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
     );
   }
 }
